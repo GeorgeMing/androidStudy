a4a88155fd7f67f90a31b03cf0392ebf5620ce70
zzm1351376761@gmail.com <zzm1351376761@gmail.com>
2017-03-10 21:55:32 +0800

    asynTask+HttpURLConnection

--- asyn/app/src/main/java/com/example/zzm/asyn/MainActivity.java
+++ asyn/app/src/main/java/com/example/zzm/asyn/MainActivity.java
@@ -6,17 +6,14 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private ProgressBar progressBar;
     private TextView textView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.text1);
-        progressBar = (ProgressBar)findViewById(R.id.progressBar02);
-        asyntest asyntestObj = new asyntest(textView, progressBar);
-        asyntestObj.execute();
+        asyntest asyntestObj = new asyntest(textView);
+        asyntestObj.execute("http://zzmyun.space");
     }
 }
--- asyn/app/src/main/java/com/example/zzm/asyn/asyntest.java
+++ asyn/app/src/main/java/com/example/zzm/asyn/asyntest.java
@@ -1,33 +1,43 @@
 package com.example.zzm.asyn;
 
 import android.os.AsyncTask;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 /**
  * Created by zzm on 2017/3/10.
  */
 
-public class asyntest extends AsyncTask<Void, Integer, Void>{
+public class asyntest extends AsyncTask<String, Integer, Void>{
     private TextView textView;
-    private ProgressBar progressBar;
+    private String responseBody;
 
-    public asyntest(TextView textView, ProgressBar progressBar) {
+    public asyntest(TextView textView) {
         this.textView = textView;
-        this.progressBar = progressBar;
     }
 
 
-
     @Override
-    protected Void doInBackground(Void... voids) {
-        NetOperator netOperator = new NetOperator();
-        int i;
-        for (i = 10; i <= 100; i+=10) {
-            netOperator.operator();
-            publishProgress(i);
+    protected Void doInBackground(String... strings) {
+        HttpURLConnection httpURLConnection;
+        try{
+            URL url = new URL(strings[0]);
+            httpURLConnection = (HttpURLConnection)url.openConnection();
+            if(httpURLConnection.getResponseCode() == 200){
+                InputStream is = httpURLConnection.getInputStream();
+                byte[] buffer = new byte[1024];
+                String str = "";
+                while(is.read(buffer)!=-1){
+                    str += new String(buffer);
+                    buffer = new byte[1024];
+                }
+                responseBody = str;//html内容
+            }
+        }catch (Exception e){
+            publishProgress(-1);
         }
         return null;
     }
@@ -42,7 +52,7 @@ public class asyntest extends AsyncTask<Void, Integer, Void>{
     @Override
     protected void onPostExecute(Void aVoid) {
         super.onPostExecute(aVoid);
-        textView.setText("asyn进程结束");
+        textView.setText(responseBody);
     }
 
 
@@ -50,6 +60,9 @@ public class asyntest extends AsyncTask<Void, Integer, Void>{
     protected void onProgressUpdate(Integer... values) {
         super.onProgressUpdate(values);
         int vlaue = values[0];
-        progressBar.setProgress(vlaue);
+        if(vlaue < 0){
+            textView.setText("http error");
+        }
+
     }
 }
