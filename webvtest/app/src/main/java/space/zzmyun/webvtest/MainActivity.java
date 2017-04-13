01d637cc7fd3bf46277dd46cb139b92953d44113
zzm1351376761@gmail.com <zzm1351376761@gmail.com>
2017-04-14 02:51:43 +0800

    test

--- webvtest/app/src/main/java/space/zzmyun/webvtest/MainActivity.java
+++ webvtest/app/src/main/java/space/zzmyun/webvtest/MainActivity.java
@@ -1,7 +1,8 @@
 package space.zzmyun.webvtest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -12,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        WebView webView = (WebView)findViewById(R.id.webview);
+        webView.loadUrl("http://192.168.199.245:8080/?action=stream");
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // TODO Auto-generated method stub
+                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                view.loadUrl(url);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        /**
+         * 设置为横屏
+         */
+        if(getRequestedOrientation()!=ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE){
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
+        super.onResume();
     }
 }
